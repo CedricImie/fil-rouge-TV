fc608da6e1b82f93ddff6b10f77188a140d31d5b
CedricImie <cedric.tougeron@nantes.imie.fr>
2016-04-08 12:41:13 +0200

    ajout ActeursController

---
+++ filRouge_CCA/src/MovieBundle/Controller/ActeursController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace MovieBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class ActeursController extends Controller
+{
+}
