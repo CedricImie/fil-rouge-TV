90824d8da03badc808f50219515998bc0f429b82
TonyIMIE <anthony.baffou@nantes.imie.fr>
2016-04-22 11:52:46 +0200

    ajout des controlleurs

---
+++ filRouge_CCA/src/MovieBundle/Controller/SeriesController.php
@@ -0,0 +1,138 @@
+<?php
+namespace MovieBundle\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use MovieBundle\Entity\Series;
+use MovieBundle\Form\SeriesType;
+/**
+ * Series controller.
+ *
+ * @Route("/series")
+ */
+class SeriesController extends Controller
+{
+    /**
+     * Lists all Series entities.
+     *
+     * @Route("/", name="series_index")
+     * @Method("GET")
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $series = $em->getRepository('MovieBundle:Series')->findAll();
+        return $this->render('series/index.html.twig', array(
+            'series' => $series,
+        ));
+    }
+    /**
+     * Creates a new Series entity.
+     *
+     * @Route("/new", name="series_new")
+     * @Method({"GET", "POST"})
+     */
+    public function newAction(Request $request)
+    {
+        $series = new Series();
+        $form = $this->createForm('MovieBundle\Form\SeriesType', $series);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($series);
+            $em->flush();
+            return $this->redirectToRoute('series_show', array('id' => $series->getId()));
+        }
+        return $this->render('series/new.html.twig', array(
+            'series' => $series,
+            'form' => $form->createView(),
+        ));
+    }
+    /**
+     * Finds and displays a Series entity.
+     *
+     * @Route("/{id}", name="series_show")
+     * @Method("GET")
+     */
+    public function showAction(Series $series)
+    {
+        $deleteForm = $this->createDeleteForm($series);
+        return $this->render('series/show.html.twig', array(
+            'series' => $series,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+    /**
+     * Displays a form to edit an existing Series entity.
+     *
+     * @Route("/{id}/edit", name="series_edit")
+     * @Method({"GET", "POST"})
+     */
+    public function editAction(Request $request, Series $series)
+    {
+        $deleteForm = $this->createDeleteForm($series);
+        $editForm = $this->createForm('MovieBundle\Form\SeriesType', $series);
+        $editForm->handleRequest($request);
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($series);
+            $em->flush();
+            return $this->redirectToRoute('series_edit', array('id' => $series->getId()));
+        }
+        return $this->render('series/edit.html.twig', array(
+            'series' => $series,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+    /**
+     * Deletes a Series entity.
+     *
+     * @Route("/{id}", name="series_delete")
+     * @Method("DELETE")
+     */
+    public function deleteAction(Request $request, Series $series)
+    {
+        $form = $this->createDeleteForm($series);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($series);
+            $em->flush();
+        }
+        return $this->redirectToRoute('series_index');
+    }
+    /**
+     * Creates a form to delete a Series entity.
+     *
+     * @param Series $series The Series entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(Series $series)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('series_delete', array('id' => $series->getId())))
+            ->setMethod('DELETE')
+            ->getForm()
+        ;
+    }
+    /**
+     * find all series.
+     *
+     * @Route("/findall", name="series_findall")
+     * @Method("GET")
+     */
+    public function findAllAction(){
+    $em = $this->getDoctrine()->getManager();
+    $query = $em->createQuery('SELECT p'
+            . ' FROM MovieBundle:Series p');
+    
+    $series = $query->getResult();
+    
+    return $this->render('series/index.html.twig', array(
+            'series' => $series,
+            ));
+    }
+}
