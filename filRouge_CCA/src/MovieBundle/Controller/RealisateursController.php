c937972e645e17375cc909b61d7c21ee9345f19f
Cyril Ollivier <cyril.ollivier@nantes.imie.fr>
2016-04-28 10:58:41 +0200

    Ajout du projet

---
+++ filRouge_CCA/src/MovieBundle/Controller/RealisateursController.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace MovieBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use MovieBundle\Entity\Realisateurs;
+use MovieBundle\Form\RealisateursType;
+
+/**
+ * Realisateurs controller.
+ *
+ * @Route("/realisateurs")
+ */
+class RealisateursController extends Controller
+{
+    /**
+     * Lists all Realisateurs entities.
+     *
+     * @Route("/", name="realisateurs_index")
+     * @Method("GET")
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $realisateurs = $em->getRepository('MovieBundle:Realisateurs')->findAll();
+
+        return $this->render('realisateurs/index.html.twig', array(
+            'realisateurs' => $realisateurs,
+        ));
+    }
+
+    /**
+     * Creates a new Realisateurs entity.
+     *
+     * @Route("/new", name="realisateurs_new")
+     * @Method({"GET", "POST"})
+     */
+    public function newAction(Request $request)
+    {
+        $realisateur = new Realisateurs();
+        $form = $this->createForm('MovieBundle\Form\RealisateursType', $realisateur);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($realisateur);
+            $em->flush();
+
+            return $this->redirectToRoute('realisateurs_show', array('id' => $realisateur->getId()));
+        }
+
+        return $this->render('realisateurs/new.html.twig', array(
+            'realisateur' => $realisateur,
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a Realisateurs entity.
+     *
+     * @Route("/{id}", name="realisateurs_show")
+     * @Method("GET")
+     */
+    public function showAction(Realisateurs $realisateur)
+    {
+        $deleteForm = $this->createDeleteForm($realisateur);
+
+        return $this->render('realisateurs/show.html.twig', array(
+            'realisateur' => $realisateur,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing Realisateurs entity.
+     *
+     * @Route("/{id}/edit", name="realisateurs_edit")
+     * @Method({"GET", "POST"})
+     */
+    public function editAction(Request $request, Realisateurs $realisateur)
+    {
+        $deleteForm = $this->createDeleteForm($realisateur);
+        $editForm = $this->createForm('MovieBundle\Form\RealisateursType', $realisateur);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($realisateur);
+            $em->flush();
+
+            return $this->redirectToRoute('realisateurs_edit', array('id' => $realisateur->getId()));
+        }
+
+        return $this->render('realisateurs/edit.html.twig', array(
+            'realisateur' => $realisateur,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a Realisateurs entity.
+     *
+     * @Route("/{id}", name="realisateurs_delete")
+     * @Method("DELETE")
+     */
+    public function deleteAction(Request $request, Realisateurs $realisateur)
+    {
+        $form = $this->createDeleteForm($realisateur);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($realisateur);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('realisateurs_index');
+    }
+
+    /**
+     * Creates a form to delete a Realisateurs entity.
+     *
+     * @param Realisateurs $realisateur The Realisateurs entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(Realisateurs $realisateur)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('realisateurs_delete', array('id' => $realisateur->getId())))
+            ->setMethod('DELETE')
+            ->getForm()
+        ;
+    }
+}
---
+++ filRouge_CCA/src/MovieBundle/Entity/Episodes.php
@@ -0,0 +1,265 @@
+<?php
+
+namespace MovieBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Episodes
+ *
+ * @ORM\Table(name="episodes")
+ * @ORM\Entity(repositoryClass="MovieBundle\Repository\EpisodesRepository")
+ */
+class Episodes
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nomEpisode", type="string", length=255)
+     */
+    private $nomEpisode;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="resumeEpisode", type="text", nullable=true)
+     */
+    private $resumeEpisode;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="dureeEpisode", type="integer")
+     */
+    private $dureeEpisode;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="dateEpisode", type="datetime")
+     */
+    private $dateEpisode;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="noteMoyenneEpisode", type="integer")
+     */
+    private $noteMoyenneEpisode;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="classificationEpisode", type="string", length=255, nullable=true)
+     */
+    private $classificationEpisode;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="diffusionEpisode", type="datetime")
+     */
+    private $diffusionEpisode;
+
+    /**
+     * @var string
+     *
+     * @ORM\ManyToOne(targetEntity="Series", inversedBy="episodes")
+     */
+    private $series;
+
+    /**
+     * @var string
+     *
+     * @ORM\ManyToOne(targetEntity="Saisons", inversedBy="episodes")
+     */
+    private $saisons;
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nomEpisode
+     *
+     * @param string $nomEpisode
+     *
+     * @return Episodes
+     */
+    public function setNomEpisode($nomEpisode)
+    {
+        $this->nomEpisode = $nomEpisode;
+
+        return $this;
+    }
+
+    /**
+     * Get nomEpisode
+     *
+     * @return string
+     */
+    public function getNomEpisode()
+    {
+        return $this->nomEpisode;
+    }
+
+    /**
+     * Set resumeEpisode
+     *
+     * @param string $resumeEpisode
+     *
+     * @return Episodes
+     */
+    public function setResumeEpisode($resumeEpisode)
+    {
+        $this->resumeEpisode = $resumeEpisode;
+
+        return $this;
+    }
+
+    /**
+     * Get resumeEpisode
+     *
+     * @return string
+     */
+    public function getResumeEpisode()
+    {
+        return $this->resumeEpisode;
+    }
+
+    /**
+     * Set dureeEpisode
+     *
+     * @param integer $dureeEpisode
+     *
+     * @return Episodes
+     */
+    public function setDureeEpisode($dureeEpisode)
+    {
+        $this->dureeEpisode = $dureeEpisode;
+
+        return $this;
+    }
+
+    /**
+     * Get dureeEpisode
+     *
+     * @return int
+     */
+    public function getDureeEpisode()
+    {
+        return $this->dureeEpisode;
+    }
+
+    /**
+     * Set dateEpisode
+     *
+     * @param \DateTime $dateEpisode
+     *
+     * @return Episodes
+     */
+    public function setDateEpisode($dateEpisode)
+    {
+        $this->dateEpisode = $dateEpisode;
+
+        return $this;
+    }
+
+    /**
+     * Get dateEpisode
+     *
+     * @return \DateTime
+     */
+    public function getDateEpisode()
+    {
+        return $this->dateEpisode;
+    }
+
+    /**
+     * Set noteMoyenneEpisode
+     *
+     * @param integer $noteMoyenneEpisode
+     *
+     * @return Episodes
+     */
+    public function setNoteMoyenneEpisode($noteMoyenneEpisode)
+    {
+        $this->noteMoyenneEpisode = $noteMoyenneEpisode;
+
+        return $this;
+    }
+
+    /**
+     * Get noteMoyenneEpisode
+     *
+     * @return int
+     */
+    public function getNoteMoyenneEpisode()
+    {
+        return $this->noteMoyenneEpisode;
+    }
+
+    /**
+     * Set classificationEpisode
+     *
+     * @param string $classificationEpisode
+     *
+     * @return Episodes
+     */
+    public function setClassificationEpisode($classificationEpisode)
+    {
+        $this->classificationEpisode = $classificationEpisode;
+
+        return $this;
+    }
+
+    /**
+     * Get classificationEpisode
+     *
+     * @return string
+     */
+    public function getClassificationEpisode()
+    {
+        return $this->classificationEpisode;
+    }
+
+    /**
+     * Set diffusionEpisode
+     *
+     * @param \DateTime $diffusionEpisode
+     *
+     * @return Episodes
+     */
+    public function setDiffusionEpisode($diffusionEpisode)
+    {
+        $this->diffusionEpisode = $diffusionEpisode;
+
+        return $this;
+    }
+
+    /**
+     * Get diffusionEpisode
+     *
+     * @return \DateTime
+     */
+    public function getDiffusionEpisode()
+    {
+        return $this->diffusionEpisode;
+    }
+}
+
